33adb4c3c67c6975d19dd0e19e8ffd876e6e3834
Felix.Hu <hulj1204@yahoo.com>
2024-09-06 16:17:59 +0800

    [U] Update home page

---
+++ src/app/main/page.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link"
+import Image from "next/image"
+
+export default function MainPage() {
+    return (
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+          <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+            <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+                Building...
+            </ol>
+    
+            <div className="flex gap-4 items-center flex-col sm:flex-row">
+              <Link
+                className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+                href="/"
+                rel="noopener noreferrer"
+              >
+                Back
+              </Link>
+            </div>
+          </main>
+        </div>
+      )
+}
