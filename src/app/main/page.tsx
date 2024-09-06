bac0c2306cf8e5eb79d5f011fafaf7a46de1ecbb
Felix.Hu <hulj1204@yahoo.com>
2024-09-06 16:27:11 +0800

    [U] Clean code

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link"
-import Image from "next/image"
 
 export default function MainPage() {
     return (
@@ -8,7 +7,6 @@ export default function MainPage() {
             <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
                 Building...
             </ol>
-    
             <div className="flex gap-4 items-center flex-col sm:flex-row">
               <Link
                 className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
