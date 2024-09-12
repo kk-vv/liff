0a6711a74fab4d0197d0af90a8a71e7214729889
Felix.Hu <hulj1204@yahoo.com>
2024-09-12 16:21:13 +0800

    [A] Add icon

--- src/app/planets/page.tsx
+++ src/app/planets/page.tsx
@@ -40,7 +40,14 @@ const PlanetsList = async() => {
 
     return (
         <div className="grid grid-cols-4 bg-red-50 min-h-full">
-            <div className="col-span-1 bg-slate-100">First Grid</div>
+            <div className="col-span-1 bg-slate-100">
+                <button className="text-orange-400 font-thin text-sm first-letter:uppercase px-2 m-2 border border-orange-400 -rotate-12 cursor-pointer bg-slate-200 rounded-full">
+                    <svg className="w-4" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"></path>
+                    </svg>
+                    login
+                </button>
+            </div>
             {/* col-span-3 标识该列占三格 */}
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-0 col-span-3 bg-slate-400"> 
             {
