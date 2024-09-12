819eadeff608bf40d3fb52a44ad64ac1635b0c32
Felix.Hu <hulj1204@yahoo.com>
2024-09-12 15:59:24 +0800

    [U] Update to full screnn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,10 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <main className="absolute h-full w-full">
+          {children}
+        </main>
       </body>
     </html>
   );
--- src/app/planets/page.tsx
+++ src/app/planets/page.tsx
@@ -39,22 +39,47 @@ const PlanetsList = async() => {
     // )   
 
     return (
-        planets.map((planet: Planet) => {
-            return (
-                <div className="mx-4 mt-5 relative overflow-hidden rounded">
-                <div className="flex items-center shadow-m h-32 lg:h-60 shadow-slate-400 bg-teal-600 text-white font-semibold space-x-2">
-                    <Image src={ImageUtils.hodler} alt={planet.name} width={100} className="h-full" style={{objectFit: "cover"}}></Image>
-                    <div>
-                        <span>{planet.name}</span>
-                        <span className="text-sm block uppercase">{planet.climate} {planet.terrain}</span>
-                    </div>
-                </div>
-                <div className="bg-white shadow-inner shadow-amber-200 text-orange-400 text-xs uppercase font-bold rounded-full p-1 absolute top-0 right-0 mt-1 mr-1">
-                    <span>Gravity: {planet.gravity}</span>
-                </div>
-                </div>
-            )    
-        })
+        <div className="grid grid-cols-4 bg-red-50 min-h-full">
+            <div className="col-span-1 bg-slate-100">First Grid</div>
+            {/* col-span-3 标识该列占三格 */}
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-0 col-span-3 bg-slate-400"> 
+            {
+                planets.map((planet: Planet, index: number) => {
+                    return (
+                        <div className="mx-4 mt-5 relative overflow-hidden rounded">
+                        <div className="flex items-center shadow-m h-32 lg:h-60 shadow-slate-400 bg-teal-600 text-white font-semibold space-x-2">
+                            <Image src={ImageUtils.hodler} alt={planet.name} width={100} className="h-full" style={{objectFit: "cover"}}></Image>
+                            <div>
+                                <span>{planet.name}</span>
+                                <span className="text-sm block uppercase">{planet.climate} {planet.terrain}</span>
+                            </div>
+                        </div>
+                        <div className="bg-white shadow-inner shadow-amber-200 text-orange-400 text-xs uppercase font-bold rounded-full p-1 absolute top-0 right-0 mt-1 mr-1">
+                            <span>Gravity: {planet.gravity}</span>
+                        </div>
+                        </div>
+                    )    
+                })
+            }
+            </div>
+        </div>
+        // card style
+        // planets.map((planet: Planet) => {
+        //     return (
+        //         <div className="mx-4 mt-5 relative overflow-hidden rounded">
+        //         <div className="flex items-center shadow-m h-32 lg:h-60 shadow-slate-400 bg-teal-600 text-white font-semibold space-x-2">
+        //             <Image src={ImageUtils.hodler} alt={planet.name} width={100} className="h-full" style={{objectFit: "cover"}}></Image>
+        //             <div>
+        //                 <span>{planet.name}</span>
+        //                 <span className="text-sm block uppercase">{planet.climate} {planet.terrain}</span>
+        //             </div>
+        //         </div>
+        //         <div className="bg-white shadow-inner shadow-amber-200 text-orange-400 text-xs uppercase font-bold rounded-full p-1 absolute top-0 right-0 mt-1 mr-1">
+        //             <span>Gravity: {planet.gravity}</span>
+        //         </div>
+        //         </div>
+        //     )    
+        // })
     )
     
     // return planets.map((planet: Planet) => {
